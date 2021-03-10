1c42ccb862c5d15a4a8c8305f65cfe80fed77b2a
DESKTOP-2RSLMSA\82103 <rud0243@naver.com>
2021-03-10 14:48:30 +0900

    first

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React, { Suspense } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Auth from './TEA/common/util/hoc/auth';
+// pages for this product
+import LandingPage from './TEA/common/layout/LandingPage/LandingPage';
+import LoginPage from './TEA/common/member/page/LoginPage/LoginPage';
+import RegisterPage from './TEA/common/member/page/RegisterPage/RegisterPage';
+import NavBar from './TEA/common/layout/NavBar/NavBar';
+import Footer from './TEA/common/layout/Footer/Footer';
+
+//null   Anyone Can go inside
+//true   only logged in user can go inside
+//false  logged in user can't go inside
+
+function App() {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <NavBar />
+            <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
+                <Switch>
+                    <Route exact path="/" component={Auth(LandingPage, null)} />
+                    <Route exact path="/login" component={Auth(LoginPage, false)} />
+                    <Route exact path="/register" component={Auth(RegisterPage, false)} />
+                </Switch>
+            </div>
+            <Footer />
+        </Suspense>
+    );
+}
+
+export default App;
