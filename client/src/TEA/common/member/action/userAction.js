7ce50ee18bd061b5489341e687e19f32107563eb
DESKTOP-2RSLMSA\82103 <rud0243@naver.com>
2021-03-10 18:01:11 +0900

    로그인고치기

--- client/src/TEA/common/member/action/userAction.js
+++ client/src/TEA/common/member/action/userAction.js
@@ -1,38 +1,50 @@
-import axios from '../../util/api';
-import { LOGIN_USER, REGISTER_USER, AUTH_USER, LOGOUT_USER } from './userActionType';
+import axios from 'axios';
+import {
+  LOGIN_USER,
+  REGISTER_USER,
+  AUTH_USER,
+  LOGOUT_USER,
+} from './userActionType';
+export const registerUser = async (dataToSubmit) => {
+  const request = await axios
+    .post(`/api/users/register`, dataToSubmit)
+    .then((response) => response.data);
 
-export function registerUser(dataToSubmit) {
-    const request = axios.post(`/api/users/register`, dataToSubmit).then((response) => response.data);
+  return {
+    type: REGISTER_USER,
+    payload: request,
+  };
+};
 
-    return {
-        type: REGISTER_USER,
-        payload: request,
-    };
-}
+export const loginUser = async (dataToSubmit) => {
+  const request = await axios
+    .post(`/api/users/login`, dataToSubmit)
+    .then((response) => response.data);
+  console.log('loginUser', request);
+  return {
+    type: LOGIN_USER,
+    payload: request,
+  };
+};
 
-export function loginUser(dataToSubmit) {
-    const request = axios.post(`/api/users/login`, dataToSubmit).then((response) => response.data);
+export const auth = async () => {
+  const request = await axios
+    .get('/api/users/auth')
+    .then((response) => response.data);
+  console.log('auth', request);
+  return {
+    type: AUTH_USER,
+    payload: request,
+  };
+};
 
-    return {
-        type: LOGIN_USER,
-        payload: request,
-    };
-}
+export const logoutUser = async () => {
+  const request = await axios
+    .get(`/api/users/logout`)
+    .then((response) => response.data);
 
-export function auth() {
-    const request = axios.get('/api/users/auth').then((response) => response.data);
-
-    return {
-        type: AUTH_USER,
-        payload: request,
-    };
-}
-
-export function logoutUser() {
-    const request = axios.get(`/api/users/logout`).then((response) => response.data);
-
-    return {
-        type: LOGOUT_USER,
-        payload: request,
-    };
-}
+  return {
+    type: LOGOUT_USER,
+    payload: request,
+  };
+};
